bf6976db8eaae252cd73d6f77a528c07dd04c135
Diego N T Araujo <diegonascimentoteixeiraaraujo@gmail.com>
2025-12-09 09:10:35 -0300

    Add Steam package

--- nixos/home.nix
+++ nixos/home.nix
@@ -8,17 +8,6 @@ in
   home.username = "diego";
   home.homeDirectory = "/home/diego";
 
-  home.file.".config/hypr/hyprland.conf".source = "${dotfiles_folder}/hypr/hyprland.conf";
-  home.file.".config/hypr/hyprlock.conf".source = "${dotfiles_folder}/hypr/hyprlock.conf";
-  home.file.".config/hypr/hypridle.conf".source = "${dotfiles_folder}/hypr/hypridle.conf";
-  home.file.".config/hypr/hyprpaper.conf".source = "${dotfiles_folder}/hypr/hyprpaper.conf";
-  home.file.".config/waybar/config.jsonc".source = "${dotfiles_folder}/waybar/config.jsonc";
-  home.file.".config/waybar/style.css".source = "${dotfiles_folder}/waybar/style.css";
-  home.file.".config/rofi/config.rasi".source = "${dotfiles_folder}/rofi/config.rasi";
-  
-  programs.firefox.enable = true;
-  programs.vscode.enable = true;
-
   gtk = {
     enable = true;
     theme = {
@@ -36,5 +25,20 @@ in
     };
   };
 
+  home.file.".config/hypr/hyprland.conf".source = "${dotfiles_folder}/hypr/hyprland.conf";
+  home.file.".config/hypr/hyprlock.conf".source = "${dotfiles_folder}/hypr/hyprlock.conf";
+  home.file.".config/hypr/hypridle.conf".source = "${dotfiles_folder}/hypr/hypridle.conf";
+  home.file.".config/hypr/hyprpaper.conf".source = "${dotfiles_folder}/hypr/hyprpaper.conf";
+  home.file.".config/waybar/config.jsonc".source = "${dotfiles_folder}/waybar/config.jsonc";
+  home.file.".config/waybar/style.css".source = "${dotfiles_folder}/waybar/style.css";
+  home.file.".config/rofi/config.rasi".source = "${dotfiles_folder}/rofi/config.rasi";
+  
+  programs.firefox.enable = true;
+  programs.vscode.enable = true;
+
+  home.packages = with pkgs; [
+    steam
+  ];
+  
   home.stateVersion = "25.11";
 }
