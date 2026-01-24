2ee42a4cc55b73cb9d83e2cdebf616c7e10b0ac3
DiegoNTAraujo <diegonascimentoteixeiraaraujo@gmail.com>
2026-01-24 16:52:41 -0300

    feat(rofi): add logout option to power menu

--- rofi/power-menu/script.sh
+++ rofi/power-menu/script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-chosen=$(printf " Power Off\n Restart\n Lock" | rofi -dmenu -p "" -i -theme .dotfiles/rofi/power-menu/config.rasi)
+chosen=$(printf " Power Off\n Restart\n Logout\n Lock" | rofi -dmenu -p "" -i -theme .dotfiles/rofi/power-menu/config.rasi)
 
 case "$chosen" in
   " Power Off") poweroff ;;
   " Restart") reboot ;;
+  " Logout") hyprctl dispatch exit ;;
   " Lock") hyprlock ;;
   *) exit 1 ;;
 esac
